f6fdefce1d737b249299c7ae52612b67e78ec26a
Sarthak Dhakal <sarthakdhakal03@gmail.com>
2021-09-17 11:52:19 +0545

    commits with pending errors

---
+++ src/components/HeroImage/HeroImage.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    background : linear-gradient(
+        to bottom , rgba(0, 0, 0, 0.5) 
+    
+        rgba(0, 0, 0, 0) 41%,
+        )    
+`;
+export const Content = styled.div``;
+export const Text = styled.div``;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,23 @@
 import React, { useState, useEffect } from "react";
-import API from "../API";
+import HeroImage from "./HeroImage";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
+import { useHomeFetch } from "../Hooks/useHomeFetch";
+
+
 import NullImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const { state, loading, error } = useHomeFetch();
+  // console.log(state);
+  console.log(state.results[0])
+  return (
+    <>
+      {state.results[0] ? (<HeroImage
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+        title={state.results[0].original_title}
+        text={state.results[0].overview} />) : null}</>
 
-  const fetchMovies = async (page, searchTerm = "") => {
-    try {
-      setError(false);
-      setLoading(true);
-      const movies = await API.fetchMovies(searchTerm, page);
-
-      setState((prev) => ({
-        ...movies,
-        results:
-          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
-      }));
-    } catch (error) {
-      setError(true);
-    }
-    setLoading(true);
-  };
-  useEffect(() => {
-    fetchMovies(1);
-  }, []);
-  console.log(state);
-  return <div>Home Page</div>;
+  )
 };
 
 export default Home;
